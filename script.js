75ae157e75513a2441c292904ebb40a4e6e5485f
Karen Cristina <94705849+karencr2@users.noreply.github.com>
2025-02-03 14:16:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);   e
+const db = getFirestore(app);   
 const analytics = getAnalytics(app);   
 
 // Função para enviar os dados para o Firestore
